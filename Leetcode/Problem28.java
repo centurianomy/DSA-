ca293cf80c7a2dcc3a7666ecc8c8e2b52f02f8a9
centurianomy <rawatchirag333@gmail.com>
2026-03-19 20:54:13 +0530

    update

--- Leetcode/Problem28.java
+++ Leetcode/Problem28.java
@@ -3,7 +3,9 @@ import java.util.*;
 class Problem28 {
     public int strStr(String haystack, String needle) {
         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            // Step 1: check if the first character matches
             if (haystack.charAt(i) == needle.charAt(0)) {
+                //compares the substring of haystack with needle--> syntx: string.substring(startIndex, endIndex) where endIndex is exclusive
                 if (haystack.substring(i, needle.length() + i).equals(needle)) {
                     return i;
                 }
