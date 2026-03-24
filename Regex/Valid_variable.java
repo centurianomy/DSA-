6c572e33189edda72e8660e0a620f17b809b7bca
centurianomy <rawatchirag333@gmail.com>
2026-03-24 23:55:01 +0530

    First of a grammer

--- Regex/Valid_variable.java
+++ Regex/Valid_variable.java
@@ -1,11 +1,19 @@
+//valid variable checker in java using regex
 public class ValidVariable {
     public static void main(String[] args) {
         String str = "var_123";   // change input here
 
-        if (str.matches("[a-zA-Z][a-zA-Z0-9_]*")) {
+        if (str.matches("[a-zA-Z][a-zA-Z0-9_]*")) //check if the string starts with a letter followed by letters, digits, or underscores
+        {
             System.out.println("Valid Variable");
         } else {
             System.out.println("Invalid Variable");
         }
     }
 }
+
+/*
+    *- allows empty string as well as one or more occurrences of the preceding character or group
+    +- doesnt allow empty spaces rest allows one or more occurrences of the preceding character or group
+    ?- allows zero or one occurrences only (multiple occurences not allowed) 
+*/
